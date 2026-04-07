e965bfc75a31f35c5a2ded4b58eecf8115a0b16a
Shalini Gupta <shalinigupta.psit@gmail.com>
2026-04-07 15:50:16 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,42 +4,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Check if there is an integer to read
         if (!sc.hasNextInt()) return;
-        
         int n = sc.nextInt();
+
         int[] arr = new int[n];
 
-        // Read n integers into an array
         for (int i = 0; i < n; i++) {
-            if (sc.hasNextInt()) {
-                arr[i] = sc.nextInt();
-            }
+            arr[i] = sc.nextInt();
         }
 
         int swaps = 0;
-        // Implement BUBBLE SORT manually
+        boolean swapped;
+
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+
                     swaps++;
+                    swapped = true;
                 }
             }
+
+            // Optimization: stop if already sorted
+            if (!swapped) break;
         }
 
-        // Print sorted array ascending, space-separated
+        // Print sorted array
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + (i == n - 1 ? "" : " "));
+            System.out.print(arr[i]);
+            if (i != n - 1) System.out.print(" ");
         }
-        
-        // Final Output
+
         System.out.println();
         System.out.println("Swaps: " + swaps);
-        
-        sc.close();
     }
 }
